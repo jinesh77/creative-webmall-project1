b1297f7f9eab734e2655f474b24ed57d9a2bdf7f
jinesh <jineshasokan77@gmail.com>
2025-04-21 12:12:48 +0530

    bootstrap website css

---
+++ js/smooth-scroll.js
@@ -0,0 +1,15 @@
+
+        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+          anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            
+            const target = document.querySelector(this.getAttribute('href'));
+            if (target) {
+              window.scrollTo({
+                top: target.offsetTop - 80, // Adjust offset for fixed navbar
+                behavior: 'smooth'
+              });
+            }
+          });
+        });
+    
